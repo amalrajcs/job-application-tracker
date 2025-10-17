4d6ab6825ac5625bfb5d957e4681d153b25c141a
Amal Raj <amalrajcs712@gmail.com>
2025-10-17 16:42:22 +0530

    axois updated

--- client/src/api/api.js
+++ client/src/api/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 
-const API = axios.create({ baseURL: import.meta.env.VITE_API_BASE || 'http://localhost:5000/api' });
+const API = axios.create({ baseURL: import.meta.env.VITE_API_BASE || 'https://job-application-tracker-theta-lime.vercel.app/' });
 
 
 export default API;
